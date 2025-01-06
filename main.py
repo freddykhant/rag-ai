d3b8b3b525d1329b44fec9c5a808afbb03328f76
Freddy Khant <freddyhmk@gmail.com>
2025-01-06 10:33:43 +0800

    initial rag concept

---
+++ main.py
@@ -0,0 +1,71 @@
+from langchain_ollama import ChatOllama
+from langchain_community.document_loaders import CSVLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import SKLearnVectorStore
+from langchain_nomic.embeddings import NomicEmbeddings
+from langchain_core.messages import HumanMessage
+
+# load LLM model
+local_llm = "llama3.2:3b"
+llm = ChatOllama(model=local_llm, temperature=0)
+llm_json_mode = ChatOllama(model=local_llm, temperature=0, output_format="json")
+
+# vector store
+files = [
+  "data/coffee_heaven_sales.csv",
+  "data/tech_emporium_sales.csv",
+  "data/green_grocers_sales.csv"
+]
+
+# load documents
+docs = []
+for file in files:
+  loader = CSVLoader(file)
+  docs += loader.load()
+
+# split documents
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+  chunk_size=1000, chunk_overlap=200
+)
+doc_splits = text_splitter.split_documents(docs)
+
+# add to vector database
+vectorstore = SKLearnVectorStore.from_documents(
+  documents=doc_splits,
+  embedding=NomicEmbeddings(model="nomic-embed-text-v1.5", inference_mode="local")
+)
+
+# create retriever
+k = min(3, len(doc_splits)) # ensure k does not exceed available chunks
+retriever = vectorstore.as_retriever(k=k)
+
+# helper method to format documents
+def format_docs(docs):
+  return "\n\n".join([doc.page_content for doc in docs])
+
+# prompt
+rag_prompt = """You are an assistant for question-answering tasks. 
+
+Here is the context to use to answer the question:
+
+{context} 
+
+Think carefully about the above context. 
+
+Now, review the user question:
+
+{question}
+
+Provide an answer to these questions using only the above context. 
+
+Use three sentences maximum and keep the answer concise.
+
+Answer:"""
+
+question = "What is the total sales for Coffee Heaven?"
+docs = retriever.invoke(question)
+docs_txt = format_docs(docs)
+rag_prompt_formatted = rag_prompt.format(context=docs_txt, question=question)
+generation = llm.invoke([HumanMessage(content=rag_prompt_formatted)])
+print("\n")
+print(generation.content)
