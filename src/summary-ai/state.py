b93365c2c1edd6e413d2f99d0a6671a7029f2bcf
Freddy Khant <freddyhmk@gmail.com>
2025-01-07 10:33:46 +0800

    declaration of state classes as dataclasses

--- src/summary-ai/state.py
+++ src/summary-ai/state.py
@@ -1,9 +1,15 @@
 import operator
 from typing_extensions import TypedDict, Annotated, List
+from dataclasses import dataclass, field
 
+@dataclass(kw_only=True)
 class SummaryState:
-  topic : str
-  generation: str
-  query: str
-  loop_step: Annotated[int, operator.add]
-  documents: List[str]
+  topic : str = field(default="None")
+  generation: str = field(default="None")
+  query: str = field(default="None")
+  loop_step: Annotated[int, operator.add] = field(default_factory=list)
+  documents: List[str] = field(default_factory=list)
+
+@dataclass(kw_only=True)
+class SummaryStateInput(TypedDict):
+  topic : str = field(default="None")
